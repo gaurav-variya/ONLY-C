aaac2d7f077dea1e04da305628227c0c7284aa82
GAURAV VARIYA <156924500+gaurav-variya@users.noreply.github.com>
2024-01-25 18:00:10 +0530

    c-language

---
+++ CH-1/1-1/1-1[1].c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+main()
+{
+    
+	printf("hello everyone !!\n\n");
+	printf("Name\t: Dhruvik variya\n");
+	printf("Age\t: 19\n");
+	printf("School\t: P.M Bhagat\n");
+	
+}
---
+++ CH-1/1-1/1-1[4].c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#define P printf
+
+int main()
+{
+	P("*\n");
+	P("*\n");
+	P("*\n");
+	P("*\t* *\t *\n");
+	P("*      *   *    *\n");
+	P("*     *     *  *\n");
+	P("*    *        * \n");
+	P("*   *\n");
+	P("*  *\n");
+	P("*\n");
+	
+}
