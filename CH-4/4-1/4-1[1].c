f3c4d9f0d6626854590836e2bf7b6199408d6e26
GAURAV VARIYA <156924500+gaurav-variya@users.noreply.github.com>
2024-01-25 18:03:42 +0530

    Add files via upload

---
+++ CH-4/4-1/4-1[1].c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+main()
+{
+	int a;
+	
+	printf("Enter any number : ");
+	scanf("%d",&a);
+	
+	printf("%d\n",++a);
+	printf("%d",--a);
+}
+
---
+++ CH-4/4-1/4-1[2].c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+	int a,b;
+	
+	printf("Enter A : ");
+	scanf("%d",&a);
+	
+	printf("Enter B : \n");
+	scanf("%d",&b);
+	
+	printf("Addition : %d\n",a+b);
+	printf("Subtraction : %d\n",a-b);
+	printf("Multiplication : %d\n",a*b);
+	printf("Division : %d\n",a/b);
+	printf("Modues : %d\n",a%b);
+}
