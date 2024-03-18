5e9b1e84b2f2a71a87373ee3ce861331fc79a588
GAURAV VARIYA <156924500+gaurav-variya@users.noreply.github.com>
2024-03-18 19:54:14 +0530

    PR-1[1].c

--- PR-1/PR-1[1].c
+++ PR-1/PR-1[1].c
@@ -1,15 +1,18 @@
 #include<stdio.h>
-
+#include<conio.h>
 
 
 main()
 {
 	
 	   float c;
+	   clrscr();
 	   
 	   printf("Enter Temperature In Celsius :");
 	   scanf("%f",&c);
 	   printf("f\t: %.2f",(c*9/5)+32);
+
+	   getch();
 	  
 
 }
