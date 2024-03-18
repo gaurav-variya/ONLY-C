8f36b560528aab21c6d3ac966db10466fa0a4c61
GAURAV VARIYA <156924500+gaurav-variya@users.noreply.github.com>
2024-03-18 20:04:08 +0530

    PR-1[3].c

--- PR-1/PR-1[3].c
+++ PR-1/PR-1[3].c
@@ -1,18 +1,20 @@
 #include<stdio.h>
+#include<conio.h>
 
 
 main()
 {
 
 	   float a,b,c;
-	   
+	   clrscr();
+	
 	   printf("enter value of a:");
 	   scanf("%f",&a);
 	   printf("enter value of b:");
 	   scanf("%f",&b);
 	   
 	   printf("c\t: %.2f",180-(a+b));
-	  
+	   getch();
 
 
 }
